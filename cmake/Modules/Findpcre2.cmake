92af503b4fa8621983f2a30cba700879825085a2
Seth Shelnutt <Shelnutt2@gmail.com>
2021-11-11 07:31:31 -0500

    Add cmake install support
    This is heavily inspired by https://github.com/julian-r/file-windows/pull/13

---
+++ cmake/Modules/Findpcre2.cmake
@@ -0,0 +1,26 @@
+MESSAGE("PCRE2_INSTALL_DIR: " $ENV{PCRE2_INSTALL_DIR})
+find_library(PCRE2_LIBRARIES NAMES pcre2 pcre2-8 pcre2d pcre2-8d PATHS "$ENV{PCRE2_INSTALL_DIR}/lib")
+find_library(PCRE2_POSIX_LIBRARIES NAMES pcre2-posix pcre2-posix-8 pcre2-posixd pcre2-posix-8d PATHS "$ENV{PCRE2_INSTALL_DIR}/lib")
+find_path(PCRE2_INCLUDE_DIRS pcre2.h PATHS "$ENV{PCRE2_INSTALL_DIR}/include")
+if(PCRE2_LIBRARIES AND PCRE2_INCLUDE_DIRS)
+  message(STATUS "PCRE2 libs: ${PCRE2_LIBRARIES}")
+  message(STATUS "PCRE2 posix libs: ${PCRE2_POSIX_LIBRARIES}")
+  message(STATUS "PCRE2 include directory: ${PCRE2_INCLUDE_DIRS}")
+  set(PCRE2_FOUND TRUE CACHE BOOL "Found PCRE2 libraries" FORCE)
+
+  add_library(pcre2-8 UNKNOWN IMPORTED)
+  set_target_properties(pcre2-8 PROPERTIES
+          IMPORTED_LOCATION "${PCRE2_LIBRARIES}"
+          INTERFACE_INCLUDE_DIRECTORIES "${PCRE2_INCLUDE_DIRS}"
+  )
+
+  add_library(pcre2-posix UNKNOWN IMPORTED)
+  set_target_properties(pcre2-posix PROPERTIES
+          IMPORTED_LOCATION "${PCRE2_POSIX_LIBRARIES}"
+          INTERFACE_INCLUDE_DIRECTORIES "${PCRE2_INCLUDE_DIRS}"
+  )
+else()
+  message(STATUS "PCRE2 library not found, building from source")
+  add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/pcre2)
+  set(PCRE2_FOUND TRUE CACHE BOOL "Found PCRE2 libraries" FORCE)
+endif()
